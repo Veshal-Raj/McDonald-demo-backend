7f5904071d566840c6f4c7d95c3f38ff90588624
unknown <Veshal Raj>
2025-07-17 08:42:30 +0530

    update: updated cors policy and added morgan

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import dotenv from 'dotenv';
 dotenv.config();
 import apiRouter from "./src/routes/index.js";
@@ -9,10 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
 app.use(express.json());
 
-
+app.use(morgan('dev'));
 
 app.use("/api", apiRouter);
 
